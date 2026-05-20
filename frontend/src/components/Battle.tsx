132a1269e6e7ce9286a659e29c0a62d2d888f200
Emad Ahmad <eahmad729@gmail.com>
2026-05-20 00:17:28 -0400

    updated

--- frontend/src/components/Battle.tsx
+++ frontend/src/components/Battle.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { Pokemon, Move } from '../types/pokemon';
 
 const BattleContainer = styled.div`
